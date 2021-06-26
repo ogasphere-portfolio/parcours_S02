58fce49d91ebdcb253cf4e77656eb20897a25371
Olivier <olivier@ogasphere.com>
2021-06-26 11:58:54 +0200

    Bonus1 terminé

--- exos/bonus1.php
+++ exos/bonus1.php
@@ -1,7 +1,7 @@
 <?php
 require_once '../inc/functions.php';
 ob_start();
-
+?>
 /**
  * === Bonus 1 : ALECMONE ===
  * 
@@ -25,17 +25,78 @@ ob_start();
 
 
 // à toi de jouer ! q(❂‿❂)p
+<?php
+require 'bonus1/datas/items.php';
+require 'bonus1/datas/types.php';
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <title>Bonus1</title>
+  <link rel="stylesheet" href="../css/bonus1.css">
+</head>
+<?php
+include 'bonus1/advert1.php';
+?>
+<main class="cam-shop">    
+    <section class="cam-shop__form-new-order">
+        <form action="" method="post">
+            <legend class="cam-shop__form-new-order__title">
+                Achète un Caméléon !
+            </legend>
+            <fieldset class="cam-shop__form-new-order__fieldset">
+                <div>
+                    <label for="customer-email">Ton email ici :</label><br>
+                    <input type="email" name="customer-email" id="customer-email">
+                </div>
+                <div>
+                    <label>Quel type de caméléon veux-tu :</label><br>
+                    <?php foreach ($types as $code => $typeName) : ?>
+                        <div>
+                            <label for="<?=$code ?>">
+                                <input type="radio" name="type" id="<?=$code ?>" value="<?=$code ?>">
+                                <?=$typeName ?>
+                            </label>
+                        </div>
+                    <?php endforeach ?>
+                </div>
+                <div>
+                    <label for="logo-color">La couleur de ton caméléon :</label><br>
+                    <input type="color" name="logo-color" id="logo-color" list="logo-color">
+                </div>
+                <div>
+                    <input type="submit" name="form-new-order" value="COMMANDER">
+                </div>
+            </fiedset>
+        </form>
+    </section>
+    <section class="cam-shop__item-list">
+        <?php foreach ($items as $item) : ?>
+            <figure class="cam-shop__item-list__figure">
+                <img src="bonus1/images/<?=$item['img'] ?>" alt="<?=$item['name'] ?>" title="<?=$item['name'] ?>">
+                <figcaption class="cam-shop__item-list__figure__name"><?=$item['name'] ?> <?=$item['price'] ?>&#x20a1;</figcaption>
+                <figcaption class="cam-shop__item-list__figure__desc"><?=$item['desc'] ?></figcaption>
+            </figure>
+        <?php endforeach ?>
+    </section>
+</main>
 
 
 
 
 
+<?php
+include 'bonus1/advert2.php';
 
 
+// Temps pour faire le bonus 1 : 27 mn      Cumul: 3h04
 
 
-/*
- * Affichage
- * Pas touche !
- */
+// <!-- /*
+//  * Affichage
+//  * Pas touche !
+//  */ -->
 check(12);
