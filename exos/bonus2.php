081d41c1d128127b90f9b968fc308c58c1bb4be8
Olivier <olivier@ogasphere.com>
2021-06-28 08:49:31 +0200

    Chuis content je commit bonus 2 terminé

--- exos/bonus2.php
+++ exos/bonus2.php
@@ -29,28 +29,188 @@ ob_start();
  */
 ?>
 
-<?php
 // A toi de jouer ! ʘ‿ʘ
+<?php
+require 'bonus2/datas/characters.php';
+
+// Initialisation des variables
+$long_name;
+$father;
+$mother;
+$gender;
+$age;
+$hair;
+$running_gag;
+$catch_phrase;
+$img1;
+$img2;
+
+include 'bonus2/inc/variables-etape1.php'
 ?>
 
 <!-- /* Mets ton HTML ici */ -->
 
-<section>
+<!DOCTYPE html>
+<html>
 
-    <!-- Etape 1 -->
-    
-    
-</section>
+    <head>
+        <meta charset="UTF-8">
+        <title>Bonus2</title>
+        <link rel="stylesheet" href="bonus2/css/style.css">
+    </head>
 
-<section>
+    <section class="etape1">
+
+        <!-- Etape 1 -->
+        <div class="etape1-graph">
+            <!-- Affichage de l'image-->
+            <img class="etape1-graph-image" 
+                src="bonus2/images/<?= $characters['Stan']['img1'] ?>" alt="">
+
+            <!-- Affichage du nom-->
+            </strong class="etape1-graph-longName"><?= $long_name ?>
+    
+        </div>
+        
+        <div class="etape1-identity">
+                 
+            <p class="etape1-text">
+                <!-- Affichage du descriptif-->
+                <strong>Father: </strong><?= $father ?></br>
+                <strong>Mother: </strong><?= $mother ?></br>
+                <strong>Gender: </strong><?= $gender ?></br>
+                <strong>Age: </strong><?= $age ?></br>
+                <strong>Hair: </strong><?= $hair ?></br>
+                <strong>Gag: </strong><?= $running_gag ?></br>
+                <strong>Cath phrase: </strong><?= $catch_phrase ?></br>
+        
+            </p>
+        </div>
+    </section>
+
 
     <!-- Etape 2 -->
-    
+    <section class="etape2">
 
-</section>
+        
+        <?php
+        foreach ($characters as $nom => $description) {  //parcours du tableau
+            // var_dump($numero);
+            ?>
+            <div class="post"></div>
+                <div class="etape2-graph" >
+
+                    <?php 
+
+                    // Choix au hasard de l'image à afficher
+                    $image_a_Afficher;
+                    if (isset($description['img2'])) {                          // si la seconde image existe
+                        $image_a_Afficher= $description['img'.mt_rand(1,2)];    // on choisit au hasard l'image a afficher
+                    }else {                                                     //sinon
+                        $image_a_Afficher = $description['img1'];               //on affiche la premiere image
+                    }
+
+                    ?>
+
+                    <div class="etape2-image-container" style="background-image:url(bonus2/images/<?= $image_a_Afficher?>)";>
+                        <!-- Affichage de l'image-->
+                       
+
+                        <img class="etape2-graph-image" 
+                        src="bonus2/images/<?= $image_a_Afficher?>" alt="">
+                    </div>
+                        <!-- Affichage du nom-->
+                    <div class="etape2-graph-longName">
+                        <?php if (isset ($description['long_name'])){       // Controle de l'existance de $description['long_name'] ?>
+                            </strong ><?= $description['long_name'] ?>
+                        <?php } else {                                      // Si $description['long_name'] n'existe pas on saute une ligne ?>
+                            </br> 
+                        <?php
+                        }    
+                        ?>
+                
+                    
+                    </div>
+
+                    <!-- AIE AIE AIE CE CSS QUELLE GALEEEEEEEEEEERE !!!!!! -->
+                    <div class="etape2-identity">
+                        
+                        <p class="etape2-text">
+                            <!-- Affichage du descriptif-->
+                            
+                            <?php if (isset ($description['father'])){                           // Controle de l'existance de $description['father']?>  
+                                <strong>Father: </strong><?= $description['father']  ?></br>
+                            <?php } else {                                                      // Si $description['father'] n'existe pas on saute une ligne?>
+                                </br>  
+                            <?php
+                            }    
+                            ?>
+
+                            <?php if (isset ($description['mother'])){                          // Controle de l'existance de $description['mother']?>  
+                                <strong>Mother: </strong><?= $description['mother']  ?></br>
+                            <?php } else {                                                      // Si $description['mother'] n'existe pas on saute une ligne?>
+                                </br>   
+                            <?php
+                            }    
+                            ?>
+
+                            <?php if (isset ($description['gender'])){                          // Controle de l'existance de $description['gender']?>  
+                            <strong>Gender: </strong><?= $description['gender']  ?></br>
+                            <?php } else {                                                      // Si $description['gender'] n'existe pas on saute une ligne?>
+                                </br>   
+                            <?php
+                            }    
+                            ?>
+                            <?php if (isset ($description['age'])){                             // Controle de l'existance de $description['age']?>  
+                            <strong>Age: </strong><?= $description['age']  ?></br> 
+                            <?php } else {                                                      // Si $description['age'] n'existe pas on saute une ligne?>
+                                </br>  
+                            <?php
+                            }   
+                            ?>
+
+                            <?php if (isset ($description['hair'])){                            // Controle de l'existance de $description['hair'] ?>  
+                            <strong>Hair: </strong><?= $description['hair']  ?></br>
+                            <?php } else {                                                      // Si $description['hair'] n'existe pas on saute une ligne ?>
+                                </br>   
+                            <?php
+                            } 
+                            ?>
+
+                            <?php if (isset ($description['running_gag'])){                     // Controle de l'existance de $description['running_gag'] ?>  
+                            <strong>Gag: </strong><?= $description['running_gag']  ?></br>
+                            <?php } else {                                                      // Si $description['running_gag'] n'existe pas on saute une ligne ?>
+                                </br>   
+                            <?php
+                            }   
+                            ?>
+                            <?php if (isset ($description['catch_phrase'])){                    // Controle de l'existance de $description['catch_phrase'] ?>  
+                            <strong>Cath phrase: </strong><?= $description['catch_phrase']  ?></br>
+                            <?php } else {                                                      // Si $description['catch_phrase'] n'existe pas on saute une ligne ?>
+                                </br>   
+                            <?php
+                            }    
+                            ?>
+                        </p>
+                    </div>
+            
+                </div>
+                
+                
+            </div>
+            <?php
+        } // Fin de la boucle
+        ?>
+    </section>
 
 
 <?php
+
+// Temps pour l'etape 1 : 40mn
+// Temps pour l'etape 2 : php+html : 1h , pour le CSS je ne sais pas mais au moins 3h heures j'ai surtout eu du mal sur la mise en forme 
+// Cumul : 7h40
+
+
 /*
  * Affichage
  * Pas touche !
---
+++ exos/bonus2/inc/variables-etape1.php
@@ -0,0 +1,34 @@
+<!-- // Initialisation des variables
+$long_name;
+$father;
+$mother;
+$gender;
+$age;
+$hair;
+$running_gag;
+$catch_phrase;
+$img1;
+$img2; -->
+
+<?php
+
+$long_name=$characters['Stan']['long_name'];
+
+$father=$characters['Stan']['father'];
+
+$mother=$characters['Stan']['mother'];
+
+$gender=$characters['Stan']['gender'];
+
+$age=$characters['Stan']['age'];
+
+$hair=$characters['Stan']['hair'];
+
+$running_gag=$characters['Stan']['running_gag'];
+
+$catch_phrase=$characters['Stan']['catch_phrase'];
+
+$img1=$characters['Stan']['img1'];
+
+$img2=$characters['Stan']['img2'];
+
